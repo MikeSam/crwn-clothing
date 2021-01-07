539ae382b0d89bc2755aeb1a571fe877dba62b81
Mike Samarin <mike.samarin@litmus.io>
2021-01-07 21:56:46 +0500

    Firebase: Add firebase support and authentication

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import { auth } from './firebase/firebase.utils';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up-page/sign-in-and-sign-up.component';
 
-function App() {
-  return (
-    <div>
-      <Header></Header>
-      <Switch>
-        <Route exact path='/' component={HomePage}></Route>
-        <Route exact path='/shop' component={ShopPage}></Route>
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({
+        currentUser: user
+      });
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}></Header>
+        <Switch>
+          <Route exact path='/' component={HomePage}></Route>
+          <Route exact path='/shop' component={ShopPage}></Route>
+          <Route exact path='/signin' component={SignInAndSignUpPage}></Route>
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import CustomButton from "../custom-button/custom-button.component";
+import FormInput from "../form-input/form-input.component";
+import { SignInWithGoogle } from "../../firebase/firebase.utils";
+
+import "./sign-in.styles.scss";
+
+class SignIn extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({
+      email: "",
+      password: "",
+    });
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign up with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="email"
+            label="Email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            label="Password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            required
+          />
+          <div className="buttons">
+            <CustomButton type="submit">Sign In</CustomButton>
+            <CustomButton onClick={SignInWithGoogle} isGoogleSignIn>
+              Sign In With Google
+            </CustomButton>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
