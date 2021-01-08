5adbabbca8b08f8f6f9485ace5eef4a98a9e0351
Mike Samarin <mike.samarin@litmus.io>
2021-01-09 00:36:27 +0500

    CartIcon: Show item's count on cart icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,18 +1,25 @@
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
-import { connect } from 'react-redux';
-import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { connect } from "react-redux";
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon"></ShoppingIcon>
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
-const mapsDispatchToProps = dispatch => ({
-    toggleCartHidden: () => dispatch(toggleCartHidden())
+const mapsDispatchToProps = (dispatch) => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapsDispatchToProps)(CartIcon);
+const mapToStateProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumulator, item) => accumulator + item.quantity,
+    0
+  ),
+});
+
+export default connect(mapToStateProps, mapsDispatchToProps)(CartIcon);
