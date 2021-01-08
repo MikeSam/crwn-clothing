66a1192df8ccf793928a213bcc63d2ef0cd33fd4
Mike Samarin <mike.samarin@litmus.io>
2021-01-08 19:51:47 +0500

    SignIn: Change button type

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -62,7 +62,7 @@ class SignIn extends React.Component {
           />
           <div className="buttons">
             <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton onClick={SignInWithGoogle} isGoogleSignIn>
+            <CustomButton type="button" onClick={SignInWithGoogle} isGoogleSignIn>
               Sign In With Google
             </CustomButton>
           </div>
